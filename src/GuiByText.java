0f2f6eb296172b1b3f852fc88d3769256f020462
muhammadzainiy31 <muhammadzainiy31@gmail.com>
2021-06-06 11:09:32 +0800

    Initial commit

---
+++ src/GuiByText.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class GuiByText {
+    GuiByText() {
+        setSize(640, 480);
+    }
+
+    public static void main(String[] args) {
+        GuiByText guiByText = new GuiByText();
+        guiByText.setVisible(true);
+    }
+}
+}
